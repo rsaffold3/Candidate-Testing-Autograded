c12e0aae71e841220e29862b142d58eaa7a8153c
Roy Saff <rsaffold3@gmail.com>
2024-03-29 14:19:33 -0500

    Roy Quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -19,13 +19,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-input.question("Whats your name? ")
-candidateName = input.question("Whats your name? ")
+candidateName= input.question("Whats your name? ")
+//("Whats your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-input.question("Who was the first American woman in space? ")
+
 candidateAnswer = input.question("Who was the first American woman in space? ")
 
 }
@@ -33,7 +33,11 @@ candidateAnswer = input.question("Who was the first American woman in space? ")
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer==correctAnswer){
+  console.log("Correct! ")
+} else {
+  console.log("Incorrect!")
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
