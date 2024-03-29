72f94f6c09a1eaa5a66034f9a262bc799ab906d4
Roy Saff <rsaffold3@gmail.com>
2024-03-29 10:49:46 -0500

    Roy's quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,12 +19,14 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+input.question("Whats your name? ")
+candidateName = input.question("Whats your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+input.question("Who was the first American woman in space? ")
+candidateAnswer = input.question("Who was the first American woman in space? ")
 
 }
 
@@ -42,7 +45,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello " + candidateName + ",");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
